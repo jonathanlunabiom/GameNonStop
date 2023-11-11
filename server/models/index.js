8d25b84b2416535d9bb7136c098af7f0e0972f85
jonathanlunabiom <jonny-luna@hotmail.com>
2023-11-10 23:35:47 -0600

    adding models index idea, needed to continue on the resolvers;2A/

---
+++ server/models/index.js
@@ -0,0 +1,23 @@
+//create a user, login in
+//email, username, password.
+//Order schemas // library of games, by a virutal
+// toJSON: {
+//     virtuals: true,
+//   },
+//   id: false,   orders length
+//Favorite games: [Favorite] include the Favorite schema
+
+//Category
+
+//Product, name, description, iamge, price, category
+// type: Schema.Types.ObjectId,
+// ref: 'Category',
+// required: true
+
+//Cart: purchaseDate, quantity and products
+// {
+//     type: Schema.Types.ObjectId,
+//     ref: 'Product'
+//   }
+
+//Favorites
