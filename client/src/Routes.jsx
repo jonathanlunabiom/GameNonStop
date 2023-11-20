88f50ca8b0e92b3af50bb2ec287c203638fafb59
EleazarF <eleazar.fuentes@gmail.com>
2023-11-19 19:39:05 -0600

    Wishlist route and page, and AddtoCart and AddtoWishlist buttons
    Wishlist route and page, and AddtoCart and AddtoWishlist buttons

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./assets/components/Layout";
 import Games from "./assets/components/Games";
 import Cart from "./assets/components/Cart";
+import Whishlist from "./assets/components/Wishlist";
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Games />} />
         <Route path="cart" element={<Cart />} />
+        <Route path="Wishlist" element={<Whishlist />} />
       </Route>
     </Routes>
   );
